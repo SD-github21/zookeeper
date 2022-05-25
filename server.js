ed5ed72943458c3bc231a9edbd702b53619b0521
SD-github21 <sapandonde.gmail.com>
2022-05-24 23:09:25 -0500

    added public folder and code to display and submit animal data

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const path = require('path');
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 // parse incoming string or array data
 app.use(express.urlencoded({extended: true}));
 // parse incoming JSON data
 app.use(express.json());
+// make files readily available to server
+app.use(express.static('public'));
+
 const { animals } = require('./data/animals.json');
 const e = require('express');
 
@@ -109,6 +113,22 @@ app.get('/api/animals/:id', (req, res) => {
     } else {
       res.send(404);
     }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
